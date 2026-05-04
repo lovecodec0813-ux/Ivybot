f061d253e2f075e04d328dde0b34e960ecd96197
lovecodec0813-ux <lovecodec0813@gmail.com>
2026-05-04 22:17:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
 app.post("/webhook", (req, res) => {
-  console.log("Webhook received:", req.body);
+  console.log("Webhook hit");
   res.sendStatus(200);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server running on " + port);
 });
