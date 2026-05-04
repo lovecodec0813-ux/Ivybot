fbd1e4846e930507404b820ceca889a017594d27
lovecodec0813-ux <lovecodec0813@gmail.com>
2026-05-04 22:54:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
 app.post("/webhook", (req, res) => {
   console.log("🔥 WEBHOOK HIT");
   console.log(JSON.stringify(req.body, null, 2));
-
   res.sendStatus(200);
-});
-
-app.get("/", (req, res) => {
-  res.send("OK");
 });
 
 const port = process.env.PORT || 3000;
